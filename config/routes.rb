4c566a4c6a2dad6c5c53b2e60e4a5b5a77745ce2
IamElaya <asison13@gmail.com>
2016-02-01 22:41:52 +0000

    6th commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 RottenMangoes::Application.routes.draw do
 
+  get "sessions/new"
+  get "sessions/create"
   get "users/new"
   get "users/create"
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
   # get "movies/index"
   # get "movies/show"
   # get "movies/new"
