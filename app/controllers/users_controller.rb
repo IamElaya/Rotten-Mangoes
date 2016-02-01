a00ad678b2f00a9e30e094c78b623a9d2b9cc94d
IamElaya <asison13@gmail.com>
2016-02-01 22:44:48 +0000

    7th commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to movies_path
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
     else
       render :new
     end
@@ -18,4 +18,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
   end
+  
 end
