9dba110f59a572203558bc5b29fbc57cac00612a
IamElaya <asison13@gmail.com>
2016-02-02 23:40:34 +0000

    Added admins

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160201232016) do
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "firstname"
